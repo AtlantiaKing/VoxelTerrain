35fcfc24f5aef8ccb14c684c896c52a034025a7b
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-02-01 14:27:02 +0100

    Memory optimization
    - Made the face transformation and neighbouring block positions static so they are shared between all blocks

--- Project/source/Block.cpp
+++ Project/source/Block.cpp
@@ -2,9 +2,17 @@
 #include "Block.h"
 #include "Face.h"
 
+bool dae::Block::m_Initialized{};
+dae::Matrix dae::Block::m_FaceTransformations[6]{};
+dae::Vector3Int dae::Block::m_NeighbouringBlocks[6]{};
+
 dae::Block::Block(const Vector3Int& position)
 	: m_TranslationMatrix{ Matrix::CreateTranslation(position) }
 {
+	if (m_Initialized) return;
+
+	m_Initialized = true;
+
 	for (int i{}; i <= static_cast<int>(Face::FaceDirection::BOTTOM); ++i)
 	{
 		switch (static_cast<Face::FaceDirection>(i))
@@ -12,37 +20,37 @@ dae::Block::Block(const Vector3Int& position)
 		case Face::FaceDirection::FORWARD:
 		{
 			m_FaceTransformations[i] = Matrix::CreateTranslation({ 0.0f, 0.0f, -0.5f });
-			m_NeighbouringBlocks[i] = position - Vector3Int::UnitZ;
+			m_NeighbouringBlocks[i] = -Vector3Int::UnitZ;
 			break;
 		}
 		case Face::FaceDirection::BACK:
 		{
 			m_FaceTransformations[i] = Matrix::CreateRotationY(180.0f * TO_RADIANS) * Matrix::CreateTranslation({ 0.0f, 0.0f, 0.5f });
-			m_NeighbouringBlocks[i] = position + Vector3Int::UnitZ;
+			m_NeighbouringBlocks[i] = Vector3Int::UnitZ;
 			break;
 		}
 		case Face::FaceDirection::RIGHT:
 		{
 			m_FaceTransformations[i] = Matrix::CreateRotationY(90.0f * TO_RADIANS) * Matrix::CreateTranslation({ -0.5f, 0.0f, 0.0f });
-			m_NeighbouringBlocks[i] = position - Vector3Int::UnitX;
+			m_NeighbouringBlocks[i] = -Vector3Int::UnitX;
 			break;
 		}
 		case Face::FaceDirection::LEFT:
 		{
 			m_FaceTransformations[i] = Matrix::CreateRotationY(-90.0f * TO_RADIANS) * Matrix::CreateTranslation({ 0.5f, 0.0f, 0.0f });
-			m_NeighbouringBlocks[i] = position + Vector3Int::UnitX;
+			m_NeighbouringBlocks[i] = Vector3Int::UnitX;
 			break;
 		}
 		case Face::FaceDirection::UP:
 		{
 			m_FaceTransformations[i] = Matrix::CreateRotationX(-90.0f * TO_RADIANS) * Matrix::CreateTranslation({ 0.0f, 0.5f, 0.0f });
-			m_NeighbouringBlocks[i] = position + Vector3Int::UnitY;
+			m_NeighbouringBlocks[i] = Vector3Int::UnitY;
 			break;
 		}
 		case Face::FaceDirection::BOTTOM:
 		{
 			m_FaceTransformations[i] = Matrix::CreateRotationX(90.0f * TO_RADIANS) * Matrix::CreateTranslation({ 0.0f, -0.5f, 0.0f });
-			m_NeighbouringBlocks[i] = position - Vector3Int::UnitY;
+			m_NeighbouringBlocks[i] = -Vector3Int::UnitY;
 			break;
 		}
 		}
@@ -52,10 +60,11 @@ dae::Block::Block(const Vector3Int& position)
 void dae::Block::Render(ID3D11DeviceContext* pDeviceContext, std::function<bool(const Vector3Int&)> isBlockPredicate, const Matrix& viewProjection, Face* pFace) const
 {
 	const Matrix worldViewProjection{ m_TranslationMatrix * viewProjection };
+	const Vector3Int position{ m_TranslationMatrix.GetTranslation() };
 
 	for (int i{}; i <= static_cast<int>(Face::FaceDirection::BOTTOM); ++i)
 	{
-		if (isBlockPredicate(m_NeighbouringBlocks[i])) continue;
+		if (isBlockPredicate(position + m_NeighbouringBlocks[i])) continue;
 
 		const Matrix& faceTransformation{ m_FaceTransformations[i] };
 
--- Project/source/Block.h
+++ Project/source/Block.h
@@ -15,8 +15,9 @@ namespace dae
 		void Render(ID3D11DeviceContext* pDeviceContext, std::function<bool(const Vector3Int&)> airPredicate, const Matrix& viewProjection, Face* pFace) const;
 	private:
 		Matrix m_TranslationMatrix{ Vector3::UnitX, Vector3::UnitY, Vector3::UnitZ, Vector3::Zero };
-		Matrix m_FaceTransformations[6]{};
-		Vector3Int m_NeighbouringBlocks[6]{};
+		static bool m_Initialized;
+		static Matrix m_FaceTransformations[6];
+		static Vector3Int m_NeighbouringBlocks[6];
 	};
 }
 
